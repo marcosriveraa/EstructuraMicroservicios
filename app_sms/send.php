6d15e7a64b6ab21679a2ef77f60bbad80a606064
marcosriveraa <xmarcos.rivera@atalayamining.com>
2025-04-03 08:42:03 +0200

    app_sms_json

---
+++ app_sms/send.php
@@ -0,0 +1,33 @@
+<?php
+require 'vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $mensaje = $_POST['mensaje'] ?? 'Mensaje Vacio';
+    $telefono = $_POST['telefono'] ?? 'Telefono Vacio';
+
+    $data = [
+        'mensaje' = $mensaje,
+        'telefono' = $telefono
+    ];
+
+    $json_data = json_encode($data);
+
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
+    $channel = $connection->channel();
+
+    $channel->queue_declare('cola_sms', false, false, false, false);
+
+    $msg = new AMQPMessage($json_data);
+
+    $channel->basic_publish($msg, '', 'cola_sms');
+
+    $channel->close();
+    $connection->close();
+
+    echo 'Mnesaje enviado ' . htmlspecialchars($json_data)
+} else {
+    echo 'Metodo no permitido'
+}
