5bf4ae210998aed4032a21b375329adf87e9e782
marcosriveraa <xmarcos.rivera@atalayamining.com>
2025-04-03 09:50:06 +0200

    app_sms

--- app_sms/consume.php
+++ app_sms/consume.php
@@ -2,23 +2,23 @@
 require 'vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
 $channel = $connection->channel();
 
 $channel->queue_declare('cola_sms', true, false, false, false);
 
-$mensajes = [];
-
+$mensajes = "";
 while ($msg = $channel->basic_get('cola_sms')) {
     $data = json_decode($msg->body, true);
-    echo "ID: {$data['id']} - Mensaje: {$data['mensaje']} - Teléfono: {$data['telefono']} - Fecha: {$data['fecha']} <br>";
-    $msg->ack(); 
+    $mensajes .= "📩 <strong>ID:</strong> {$data['id']} <br>";
+    $mensajes .= "✉️ <strong>Mensaje:</strong> {$data['mensaje']} <br>";
+    $mensajes .= "📞 <strong>Teléfono:</strong> {$data['telefono']} <br>";
+    $mensajes .= "⏳ <strong>Fecha:</strong> {$data['fecha']} <br><hr>";
+    $msg->ack();
 }
-
 
 $channel->close();
 $connection->close();
 
-echo json_encode($mensajes);
+echo $mensajes ?: "<p>No hay mensajes en la cola.</p>";
